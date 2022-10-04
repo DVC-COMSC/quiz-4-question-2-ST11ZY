66cca8e17176d5704f688e9dae64dceecb6b067d
Your Name <you@example.com>
2022-10-03 18:28:53 -0700

    code quiz

--- q2.py
+++ q2.py
@@ -3,7 +3,27 @@
 # Make your Code
 # ******************************
 
-
+x=[]
+maxlen=""
+minlen="00000000000000000000000000000000000"
+inp=input("String?")
+while inp!="STOP" and inp!="stop":
+  x+=[inp]
+  if len(inp)>=len(maxlen):
+      maxlen=inp
+  if len(inp)<=len(minlen):
+      minlen=inp
+  inp=input("String?")
+  while inp!="STOP" and inp!="stop":
+    if len(inp)>=len(maxlen):
+      maxlen=inp
+      break
+    if len(inp)<=len(minlen):
+      minlen=inp
+      break
+  if inp=="stop" or inp=="STOP":
+    break
+print(maxlen+" "+minlen)
 # Requirement
 # No need to use list
 # All input values are taken one by one separatively.
