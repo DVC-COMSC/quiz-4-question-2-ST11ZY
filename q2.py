fdb714182c09840c5709d94f371e19c0ff68fd1f
Kyu Lee <kwooler@gmail.com>
2022-10-09 16:32:14 -0700

    q4

--- q2.py
+++ q2.py
@@ -3,26 +3,26 @@
 # Make your Code
 # ******************************
 
-x=[]
-maxlen=""
-minlen="00000000000000000000000000000000000"
-inp=input("String?")
-while inp!="STOP" and inp!="stop":
-  x+=[inp]
-  if len(inp)>=len(maxlen):
-      maxlen=inp
-  if len(inp)<=len(minlen):
-      minlen=inp
-  inp=input("String?")
-  while inp!="STOP" and inp!="stop":
-    if len(inp)>=len(maxlen):
-      maxlen=inp
-      break
-    if len(inp)<=len(minlen):
-      minlen=inp
-      break
-  if inp=="stop" or inp=="STOP":
-    break
+x = []
+maxlen = ""
+minlen = "00000000000000000000000000000000000"
+inp = input("String?")
+while inp != "STOP" and inp != "stop":
+    x += [inp]
+    if len(inp) >= len(maxlen):
+        maxlen = inp
+    if len(inp) <= len(minlen):
+        minlen = inp
+    inp = input("String?")
+    # while inp!="STOP" and inp!="stop":
+    #   if len(inp)>=len(maxlen):
+    #     maxlen=inp
+    #     break
+    #   if len(inp)<=len(minlen):
+    #     minlen=inp
+    #     break
+    if inp == "stop" or inp == "STOP":
+        break
 print(maxlen+" "+minlen)
 # Requirement
 # No need to use list
